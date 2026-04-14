f00652a18708fc5b5e7e4363341595a9611f80e1
Rohit Taneja <tanejarohit31@gmail.com>
2026-04-14 10:00:45 -0500

    Added env variable support

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,13 @@
 # frontend/app.py
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    API= os.getenv("API")
+    return render_template("index.html", API=API)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
